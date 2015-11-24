e46deb2c26afb73acead3ce0ae2ce7d52d78cd57
bdadam <bdadam@mail.bradley.edu>
2015-11-24 12:16:26 -0600

    try this

--- load.php
+++ load.php
@@ -3,7 +3,7 @@
 $db = new PDO('mysql:host=127.0.0.1;dbname=bdadam', 'bdadam', 'jzjcXw;560');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
-$query = $db->$query("
+$query = $db->query("
 	SELECT *
 	FROM chat
 ");
